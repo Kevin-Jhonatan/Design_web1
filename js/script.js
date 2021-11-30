931e93cd6c98de36ab0cf801d00a2c01aae4da01
keevj <jhonatan72700606@gmail.com>
2021-11-30 11:53:57 -0400

    * Diseño Web 1 | 30/11/21 - Se creó toda la estructura de trabajo - Se creó un repositorio como Design_web1 - Se creó un README donde esta toda la explicación de la estructura de trabajo - Se creo una prueba en css/style.css y js/script.js para asegurarse que funciona

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* 
+    Para probar que realmente nuestro index.js esta recibiendo código JavaScript
+    de nuestro archivo script.js descomente el código de console.log("");
+    y recargue nuestro index.html en cualquier navegador y 1.- clic derecho
+    2.- inspeccionar 3.- consola y ahi podra ver Soy Kevin en inglés.
+    console.log("I am Kevin"); 
+*/
